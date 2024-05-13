8c68c23a8f3242aa8e87130c6eaf0314719762e8
jrs-neto <jrd.santosneto@gmail.com>
2024-05-13 17:51:41 -0300

    Adicionados exemplos de  Break/Continue/While/DoWhile

---
+++ estruturas-condicionais/src/ExemploBreakContinue.java
@@ -0,0 +1,12 @@
+public class ExemploBreakContinue {
+  public static void main(String[] args) {
+    for (int numero = 1; numero <= 5; numero++) {
+      if (numero == 3)
+        // break;
+        continue;
+
+      System.out.println(numero);
+
+    }
+  }
+}
---
+++ estruturas-condicionais/src/ExemploWhile.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+  public static void main(String[] args) {
+    double mesada = 50.0;
+    while (mesada > 0) {
+      Double valorDoce = valorAleatorio();
+      if (valorDoce > mesada)
+        valorDoce = mesada;
+
+      System.out.println("Doce do valor: " + valorDoce + " adicionado ao carrinho.");
+      mesada = mesada - valorDoce;
+    }
+
+    System.out.println("Mesada:" + mesada);
+    System.out.println("Joãozinho gastou toda a sua mesada em doces.");
+  }
+
+  private static double valorAleatorio() {
+    return ThreadLocalRandom.current().nextDouble(2, 8);
+  }
+}
